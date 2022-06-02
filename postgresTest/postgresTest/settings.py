80b95ade62db324ab8cfc025ef64f28bbaaa5ee2
shujun-lee <shujunn.lee@gmail.com>
2022-06-02 16:15:26 +0800

    update settings

--- postgresTest/postgresTest/settings.py
+++ postgresTest/postgresTest/settings.py
@@ -160,7 +160,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT =
+STATIC_ROOT = ''
 STATICFILES_DIRS = [
     BASE_DIR / "static",
     '/var/www/static/',
